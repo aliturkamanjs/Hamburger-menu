3b9c3e4d49720bdd2c11d6175a17c539cabeb527
ali turkaman <79016171+aliturkamanjs@users.noreply.github.com>
2021-06-16 10:09:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 window.onload = function(){
+    
     let container = document.getElementById('container')
+    
     container.addEventListener('click' , function(){
+        
         if(!container.classList.contains('play')){
             container.classList.add('play')
         } else {
+            
             container.classList.remove('play')
         }
     })
